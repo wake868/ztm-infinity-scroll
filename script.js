1801bafcdfe01f48897142765be0b89680ef1b8c
Wakeland <wakelandm@CraftsAmericana.com>
2023-02-10 07:41:44 -0800

    incremental photo load

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Unsplash API
-const count = 10;
+// let count = 5;
 const apiKey = "_1CKchQncU2Dow3jy6XdJEQD9WMd2b9e1tk9K4pVc8I";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=5`;
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper function to set attributes on DOM elements
 function setAttributes(element, attributes) {
@@ -16,6 +29,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements for links and photos
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Creating <a> to link to Unsplash
@@ -33,6 +49,9 @@ function displayPhotos() {
       title: photo.alt_description
     });
 
+    // Event listener check when each is finished loading
+    image.addEventListener("load", imageLoaded);
+
     // Put <img> inside <a>, then put both inside the imageContainer Element
     item.appendChild(image);
     imageContainer.appendChild(item);
@@ -44,12 +63,27 @@ async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
-    displayPhotos();
 
-    console.log(data);
+    displayPhotos();
   } catch (error) {}
 }
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// After the page has fully loaded with the initial set of 5 photos change count to grab to 30.
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=30`;
+});
 
 // On Load
 getPhotos();
